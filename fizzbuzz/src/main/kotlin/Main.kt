292f5840dd2a5aba9199dd01364ba00534bd838d
kota-shiokara <ikanoshiokara.fun@gmail.com>
2021-03-26 18:13:19 +0900

    fizzbuzz

---
+++ fizzbuzz/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(){
+    (1..100).map{
+                when{
+                    it % 15 == 0 -> {
+                        "fizzbuzz"
+                    }
+                    it % 3 == 0 -> {
+                        "fizz"
+                    }
+                    it % 5 == 0 -> {
+                        "buzz"
+                    }
+                    else -> { it }
+                }}
+            .forEach{
+                println(it)
+            }
+}
