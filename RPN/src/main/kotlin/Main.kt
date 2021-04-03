5f0b43136a95247b787d1760198673db6388489c
kota-shiokara <ikanoshiokara.fun@gmail.com>
2021-04-03 15:43:09 +0900

    refactor:RPN

--- RPN/src/main/kotlin/Main.kt
+++ RPN/src/main/kotlin/Main.kt
@@ -1,21 +1,26 @@
+import java.lang.ClassCastException
 import kotlin.math.ceil
 import kotlin.math.floor
 
-fun main(){
+fun main() {
     val console = System.console() ?: null
     println("逆ポーランド記法で計算したい文字列を入力してください。")
-    println("独自ルールとして「,」を使用し二桁以上の数字を扱えます。")
-    println("例：12,3+ -> 12 + 3")
+    println("数字、演算子は空白で区切ってください。")
+    println("例：12 3 + -> 12 + 3")
 
-    val formula = console?.readLine() ?: readLine() ?: ""
-    if(formula.isBlank()) return
+    while (true) {
+        val formula = console?.readLine() ?: readLine() ?: ""
+        if (formula.isBlank()) return
+        else if (formula == "exit") break
 
-    val output = RPN(formula.split(" ").joinToString(separator = ""))
-            .solve()
-            .toDouble()
-    if(floor(output) == ceil(output)) {
-        val integer = output.toString().split(".")
-        println(integer[0])
+        val output = RPN(formula.split(" ")).solve().toDouble()
+
+        if (floor(output) == ceil(output)) {
+            val integer = output.toString().split(".")
+            println(integer[0])
+        } else println("result: %.2f".format(output))
+
+
+        print("Next formula: ")
     }
-    else println("result: %.2f".format(output))
 }
